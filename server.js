1013c30cd98fe3fee2d5fc08788eba7d242538d3
sam-malanchuk <malanchuksam@gmail.com>
2019-08-13 19:34:09 -0400

    wrote logger() custom middleware

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const usersRouter = require('./users/userRouter.js');
 const server = express();
 
 server.use('/api/users', usersRouter);
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -13,7 +14,9 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  const timestamp = new Date().toLocaleString();
+  console.log(`A ${req.method} request at '${req.url}' at ${timestamp}`);
+  next();
 };
 
 module.exports = server;
